8459b853fc242752691815d97fb4eda6401194fa
hritik01478 <hritikkumar01478@gmail.com>
2022-06-04 19:12:48 +0530

    form created

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Hritik's Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: "black",
                     backgroundColor: "#ffa31a",
                     borderRadius: '8px',
                     fontWeight: '200'
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav >
     );
